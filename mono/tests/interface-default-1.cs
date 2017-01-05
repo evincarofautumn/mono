430d30369a253aa909eadeaf7403ee64c4301792
Jon Purdy <evincarofautumn@gmail.com>
2017-01-05 14:50:50 -0800

    [tests] Add test for default interface methods.

---
+++ mono/tests/interface-default-1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace System.Runtime.CompilerServices
+{
+    class DefaultImplementationAttribute : Attribute
+    {
+        public DefaultImplementationAttribute(string[] defaultImplementations)
+        {
+            DefaultImplementations = defaultImplementations;
+        }
+        public string[] DefaultImplementations { get; }
+    }
+}
+
+namespace DefaultInterfacesBasic
+{
+    [DefaultImplementationAttribute(new string[] { "DefaultImplementation"})]
+    interface IInterfaceWithDefault
+    {
+        string DefaultMethod();
+    }
+    class ClassThatExplicitlyImplementsInterface : IInterfaceWithDefault
+    {
+        public string DefaultMethod()
+        {
+            return "NotDefaultImplementation";
+        }
+    }
+    class ClassThatDoesnotImplementMethod : IInterfaceWithDefault
+    {
+        // DELETE THIS METHOD
+        public string DefaultMethod()
+        {
+            return "NotDefaultImplementation";
+        }
+    }
+    class OuterType
+    {
+        // MOVE THIS TYPE TO BE NESTED IN IInterfaceWithDefault
+        static class DefaultImplementation
+        {
+            static string DefaultMethod(IInterfaceWithDefault actualThis)
+            {
+                return "DefaultImplementation";
+            }
+        }
+    }
+    class Program
+    {
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        static string CallDefaultMethod(IInterfaceWithDefault iface)
+        {
+            return iface.DefaultMethod();
+        }
+        static int Main(string[] args)
+        {
+            if (!"NotDefaultImplementation".Equals(CallDefaultMethod(new ClassThatExplicitlyImplementsInterface())))
+                return 1;
+            if (!"DefaultImplementation".Equals(CallDefaultMethod(new ClassThatDoesnotImplementMethod())))
+                return 2;
+            return 0;
+        }
+    }
+}
